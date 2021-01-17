93a7dff6fca1f72f2381df2f74d7462c670ff69e
fj0lverkr <nils.nahooy@live.com>
2021-01-17 18:29:06 +0100

    implemented shooting

--- src/main.rs
+++ src/main.rs
@@ -52,8 +52,12 @@ fn main() -> Result <(), Box<dyn Error>>{
 
     //Game Loop
     let mut player = Player::new();
+    let mut instant = Instant::now();
+    
     'gameloop: loop {
         //Per Frame Init
+        let delta = instant.elapsed();
+        instant = Instant::now();
         let mut curr_frame = new_frame();
 
 
@@ -63,6 +67,11 @@ fn main() -> Result <(), Box<dyn Error>>{
                 match key_event.code {
                     KeyCode::Left => player.move_left(),
                     KeyCode::Right => player.move_right(),
+                    KeyCode::Char(' ') | KeyCode::Enter => {
+                        if player.shoot() {
+                            audio.play("pew");
+                        }
+                    }
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -71,6 +80,9 @@ fn main() -> Result <(), Box<dyn Error>>{
                 }
             }
         }
+        //Updates
+        player.update(delta);
+
         //Draw & render
         player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame);
